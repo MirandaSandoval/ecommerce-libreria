8a771ba0b93db5739176be2cc314a55442385a52
Miranda <mirandacandelasandovalaltuna@gmail.com>
2023-10-09 17:05:42 -0300

    Actualizacion 2

--- modelos_de_datos/User.js
+++ modelos_de_datos/User.js
@@ -1,11 +1,58 @@
 const mongoose = require('mongoose');
+const unique = require('mongoose-unique-validator');
+const validate = require('mongoose-validator');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  // Otros campos relevantes para un usuario
+const validacionNombre = [
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'El nombre no debe exceder {ARGS[1]} caracteres.'
+    })
+];
+
+const validacionEmail = [
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'Email no debe exceder {ARGS[1]} caracteres.'
+    }),
+    validate({
+        validator: 'isEmail',
+        message: 'Email debe ser valido.'
+    })
+];
+
+const ageValidator = [
+    // Validaciones...
+];
+
+const genderValidator = [
+    // validaciones...
+];
+
+// Define el modelo de base de datos
+const UserSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'Name is required.'],
+        validate: validacionNombre
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required.'],
+        unique: true,
+        validate: validacionEmail
+    },
+    edad: {
+        type: Number,
+        validate: ageValidator
+    },
+    genero: {
+        type: String,
+        validate: genderValidator
+    }
 });
+//Para que se loguee solo un usuario con una direccion de correo electronico
+UserSchema.plugin(unique, { message: 'Este {PATH} ya esta ocupado.' });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+const User = module.exports = mongoose.model('user', UserSchema);
